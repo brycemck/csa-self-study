8e7d35e0f1aa3cc415247c5a3a146dfd506989cf
Bryce McKenney <brycemckenney@gmail.com>
2017-10-09 11:38:52 -0700

    created new project

--- coin-counter/src/com/bryce/CoinCounter.java
+++ coin-counter/src/com/bryce/CoinCounter.java
@@ -13,46 +13,38 @@ package com.bryce;
 
 import javax.swing.JOptionPane;
 
-/**
- * In the real world, java managers want insane amounts of java documentation in the form of Javadocs.
- * I think this is largely because some programmers don't write expressive code that is comprehensible so instead of enforcing good code,
- * they largely rely on documentation. Don't be the guy that can't write expressive code. You don't seem to have a problem with this.
- * eventuallyyou'll want to know how to write Javadocs for your methods. Your Dad can show you how to get Checkstyle setup :)
- *
- * You are right to focus on your code first, though! 
- */
 public class CoinCounter {
 	static int inputDollars = 0;
 	static double inputCents = 0;
 	
-	private static String askForDollars() {
+	public static String askForDollars() {
 	  return JOptionPane.showInputDialog("Please input a dollar value. (x.xx format)").replace(",", ""); // remove any commas
 	}
 	
-	private static double numberOfQuartersFromDollars(int dollars) {
+	public static double numberOfQuartersFromDollars(int dollars) {
 		return dollars * 4;
 	}
-	private static double numberOfQuartersFromCents(double cents) {
+	public static double numberOfQuartersFromCents(double cents) {
 		inputCents = inputCents - (Math.floor(cents / 25) * 25);
 		return Math.floor(cents / 25);
 	}
-	private static double numberOfDimesFromCents(double cents) {
+	public static double numberOfDimesFromCents(double cents) {
 		inputCents = inputCents - (Math.floor(cents / 10) * 10);
 		return Math.floor(cents / 10);
 	}
-	private static double numberOfNickelsFromCents(double cents) {
+	public static double numberOfNickelsFromCents(double cents) {
 		inputCents = inputCents - (Math.floor(cents / 5) * 5);
 		return Math.floor(cents / 5);
 	}
 	
-	private static void parseCoins() {
+	public static void parseCoins() {
 		System.out.println("Quarters: " + (numberOfQuartersFromDollars(inputDollars) + numberOfQuartersFromCents(inputCents)));
 		System.out.println("Dimes: " + numberOfDimesFromCents(inputCents));
 		System.out.println("Nickels: " + numberOfNickelsFromCents(inputCents));
 		System.out.println("Pennies: " + inputCents);
 	}
 
-	private static boolean isDollarInputValid(String input) {
+	public static boolean isDollarInputValid(String input) {
     String[] inputSplit = input.split("\\.");
     try {
       inputDollars = Integer.parseInt(inputSplit[0]);
